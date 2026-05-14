e08ac405391d70005e869cb7a9b83117cbd45548
Vedant Hire <80911073+HireVedant@users.noreply.github.com>
2026-05-15 01:27:45 +0530

    Add files via upload
    I have improved the Store UI and added 3 different sections consisting of 1.Weapons 2.Defense 3.Special each containing 5 different types of objects for each section.
    The golden glow are legendary rank , purple are rare rank and blue ones are common rank .

---
+++ store1.hpp
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <raylib.h>
+#include <raymath.h>
+#include <string>
+#include <iostream>
+#include "Common.hpp"
+
+extern int Index_Store;
+extern int Index_Item;
+extern int Index_Select;
+
+class Store {
+public:
+	bool Show;
+
+	Rectangle Window;
+	Rectangle ItemBox;
+
+	std::string Attribute_Weapon[5];  
+	std::string Attribute_Defence[5];
+	std::string Attribute_Special[5];
+
+	int Price_Weapon[5];
+	int Rarity_Weapon[5];
+
+	int Price_Defence[5];
+	int Rarity_Defence[5];
+
+	int Price_Special[5];
+	int Rarity_Special[5];
+
+	std::string C1[5];
+	std::string C2[5];
+	std::string C3[5];
+
+	Texture2D Tex1[5];
+	Texture2D Tex2[5];
+	Texture2D Tex3[5];
+
+	Color GetRarityColor(int rarity);
+
+	Store();
+
+	void ShowWindow(bool &show);
+	void DrawWindow();
+
+	void LoadWindowAssets();
+
+	void DrawInsideWindow(int index, std::string C[5], Texture2D Tex[5]);
+
+	void DrawStoreMenuBox(Rectangle Win, Color c1 , Color c2 , Color c3 , Color c4 , int index);
+	void DrawStoreText(Rectangle Win, const char* Name, Color N, Color H, Color P);
+	void DrawItemBox(Rectangle Box, Color N, Color H, Color P , Color B, const char* Attribute , int index, int rarity);
+
+	void DrawWindowSelection(int index, std::string S[5], int Rarities[5], Color c1, Color c2, Color c3, Color c4);
+	void DrawWindowItemSelection(int index, std::string Name, std::string Context, int Rarity, int Price, Texture2D Tex);
+
+};
+extern Store store;
